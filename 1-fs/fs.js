ea5b0675c3eea8f319d25a0598316e8e131c1501
park <minpark93@gmail.com>
2024-01-11 07:58:22 -0800

    update2

--- 1-fs/fs.js
+++ 1-fs/fs.js
@@ -11,8 +11,10 @@ import fs from "node:fs"; // ES6 modules
     } else if (process.argv[2] === 'create') {
         console.log("--create a new pet--");
         createPet(); // Usage: node fs.js create AGE KIND NAME
-    }
-    else if (process.argv.length < 3) {
+    } else if (process.argv[2] === 'update') {
+        console.log("--update a pet--");
+        updatePet(); // Usage: node fs.js update INDEX AGE KIND NAME
+    } else if (process.argv.length < 3) {
         console.error('Usage: node pets.js [read | create | update | destroy]');
     } 
        
@@ -33,35 +35,71 @@ import fs from "node:fs"; // ES6 modules
                 const index = Number(process.argv[3]);
                 // check index to see if its an integer, and within the bounds of pets.length
                 if (Number.isInteger(index) && index >= 0 && index < pets.length) {
+                    // otherwise, print the pet at that index
                     console.log(pets[index]);
                 } else {
                     // if we don't have a proper index, log correct usage and exit with code 1
                     console.error(`Usage: node pets.js read INDEX`);
                     process.exit(1);
                 }
-                // otherwise, print the pet at that index
             } else {
+                // display all pets
                 console.log(pets);
             }
         })
     }
 // Usage: node fs.js create AGE KIND NAME
     function createPet() {
-        let age, kind, name;
+        // let age, kind, name;
+        const age = Number(process.argv[3]);
+        const kind = process.argv[4];
+        const name = process.argv[5];
         
-        if (process.argv[3]) {
-            // check if its number
-            age = Number(process.argv[3]);
-        }
-        if (process.argv[4]) {
-            kind = process.argv[4]; // already a string
-        }
-        if (process.argv[5]) {
-            name = process.argv[5]; // already a string
-        }
         const newPet = { age, kind, name};
         if(!age || !kind || !name) {
             console.error(`Usage: node pets.js create AGE KIND NAME`);
+            process.exit(1);
+        }
+        fs.readFile('../pets.json', 'utf8', (error, data) => {
+            if (error) {
+                throw error;
+            }
+            // let pets = [];
+            // convert the JSON to an array
+            const pets = JSON.parse(data);
+            // add the new pet to the array
+            pets.push(newPet);
+            // convert the array back to JSON
+            const updatedPets = JSON.stringify(pets);
+
+            fs.writeFile('../pets.json', updatedPets, (error) => {
+                if (error) {
+                    throw error;
+                } else {
+                    console.log(pets);
+                }
+        })
+    })}
+
+    function updatePet() {
+        let index, age, kind, name;
+        if (process.argv[3]) {
+            // check if its number
+            index = Number(process.argv[3]);
+        }
+        if (process.argv[4]) {
+            // check if its number
+            age = Number(process.argv[4]);
+        }
+        if (process.argv[5]) {
+            kind = process.argv[5]; // already a string
+        }
+        if (process.argv[6]) {
+            name = process.argv[6]; // already a string
+        }
+        const updatedPet = { age, kind, name};
+        if(!index || !age || !kind || !name) {
+            console.error(`Usage: node pets.js update INDEX AGE KIND NAME`);
             process.exit(1);
         }
         fs.readFile('../pets.json', 'utf8', (error, data) => {
@@ -72,7 +110,7 @@ import fs from "node:fs"; // ES6 modules
             // convert the JSON to an array
             pets = JSON.parse(data);
             // add the new pet to the array
-            pets.push(newPet);
+            pets[index] = updatedPet;
             // convert the array back to JSON
             const updatedPets = JSON.stringify(pets);
 
